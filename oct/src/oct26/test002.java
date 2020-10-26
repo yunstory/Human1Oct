b423baf9dfee4853e6c746079826e0a6d35e0e0b
USER <USER@USER-PC.local>
2020-10-27 08:35:35 +0900

    add

---
+++ oct/src/oct26/test002.java
@@ -0,0 +1,26 @@
+package oct26;
+
+public class test002 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// 3 6 9 ≥Ó¿Ã 
+		int count = 0;
+		
+		for(int i=1; i<=50; i++) {
+			if(i==3 || i==6 || i==9) {
+				System.out.println("¬¶");
+			}else {
+				System.out.println(i);
+				count++;
+			}
+		}
+		
+		
+		
+
+	}
+
+}

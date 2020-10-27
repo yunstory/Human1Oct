91c0baead05afc66f29f848bec85be83d6d0a434
USER <USER@USER-PC.local>
2020-10-27 15:55:53 +0900

    star

--- oct/src/oct27/Star01.java
+++ oct/src/oct27/Star01.java
@@ -1,5 +1,7 @@
 package oct27;
 
+import java.util.Scanner;
+
 public class Star01 {
 
 	public static void main(String[] args) {
@@ -14,7 +16,7 @@ public class Star01 {
 		}
 		
 		
-		System.out.println("--------------------------1STAR");
+		System.out.println("--------------------------¡â1STAR");
 		
 		
 		for(int i=1; i<=9; i++) {
@@ -28,7 +30,7 @@ public class Star01 {
 		}
 		
 		
-		System.out.println("--------------------------2STAR");
+		System.out.println("--------------------------¡â2STAR");
 
 		for(int i=1; i<=9; i++) {
 			for(int j=1; j<=9-i; j++) {
@@ -37,7 +39,7 @@ public class Star01 {
 			System.out.println();
 		}
 		
-		System.out.println("--------------------------3STAR");
+		System.out.println("--------------------------¡â3STAR");
 		
 		for(int i=1; i<=9; i++) {
 			for(int j=1; j<=i-1; j++) {
@@ -49,7 +51,7 @@ public class Star01 {
 			System.out.println();
 		}
 		
-		System.out.println("--------------------------4STAR");
+		System.out.println("--------------------------¡â4STAR");
 		
 		for(int i=1; i<=9; i++) {
 			if(i<=5) {
@@ -64,7 +66,7 @@ public class Star01 {
 			System.out.println();
 		}
 		
-		System.out.println("--------------------------5STAR");
+		System.out.println("--------------------------¡â5STAR");
 		
 		
 		for(int i=1; i<=9; i++) {
@@ -88,7 +90,7 @@ public class Star01 {
 			System.out.println();
 		}
 		
-		System.out.println("--------------------------6STAR");
+		System.out.println("--------------------------¡â6STAR");
 
 		
 		for(int i=1; i<=9; i++) {
@@ -104,18 +106,150 @@ public class Star01 {
 			System.out.println();
 		}
 
-		System.out.println("--------------------------7STAR");
+		System.out.println("--------------------------¡â7STAR");
 		
 		for(int i=1; i<=9; i++) {
-			for(int j=1; j<=i-1; j++) {
+			if(i<=5) {
+				for(int j=1; j<=i-1; j++) {
+					System.out.print(" ");
+				}
+				for(int k=0; k<=5-i; k++) {
+					System.out.print("*");
+				}
+			}else {
+				for(int j=1; j<=9-i; j++) {
+					System.out.print(" ");
+				}
+				for(int k=0; k<=i-5; k++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("--------------------------¡â8STAR");
+		
+		
+		for(int i=1; i<=9; i++) {
+			if(i<=5) {
+				for(int j=0; j<=5-i; j++) {
+					System.out.print("*");
+				}
+				for(int k=0; k<=i*2-2; k++) {
+					System.out.print(" ");
+				}
+				for(int j=0; j<=5-i; j++) {
+					System.out.print("*");
+				}
+			}else {
+				for(int j=0; j<=i-5; j++) {
+					System.out.print("*");
+				}
+				for(int k=0; k<=(9-i)*2; k++) {
+					System.out.print(" ");
+				}
+				for(int j=0; j<=i-5; j++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+		
+		System.out.println("--------------------------¡â9STAR");
+		
+		
+		for(int i=1; i<=9; i++) {
+			for(int j=1; j<=9-i; j++) {
 				System.out.print(" ");
 			}
-			for(int k=0; k<=5-i; k++) {
+			for(int k=1; k<=i*2-1; k++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 		
-		System.out.println("--------------------------8STAR");
+		
+		System.out.println("--------------------------¡â10STAR");
+		
+		
+		for(int i=1; i<=9; i++) {
+			for(int k=1; k<=i-1; k++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<=(9-i)*2; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("--------------------------¡â11STAR");
+		
+		for(int i=1; i<=9; i++) {
+			if(i<=5) {
+				for(int k=1; k<=i-1; k++) {
+					System.out.print(" ");
+				}
+				for(int j=0; j<=(5-i)*2; j++) {
+					System.out.print("*");
+				}
+			}else {
+				for(int k=1; k<=9-i; k++) {
+					System.out.print(" ");
+				}
+				for(int j=0; j<=(i-5)*2; j++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("--------------------------¡â12STAR");
+		
+
+		for(int i=1; i<=9; i++) {
+			if(i<=5) {
+				for(int k=0; k<=5-i; k++) {
+					System.out.print(" ");
+				}
+				for(int j=1; j<=4; j++) {
+					System.out.print("*");
+				}
+			}else {
+				for(int k=0; k<=i-5; k++) {
+					System.out.print(" ");
+				}
+				for(int j=1; j<=4; j++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		
+		for(int i=1; i<=9; i++) {
+			if(i<=5) {
+				for(int k=1; k<=i-1; k++) {
+					System.out.print(" ");
+				}
+				for(int j=1; j<=4; j++) {
+					System.out.print("*");
+				}
+			}else {
+				for(int k=1; k<=9-i; k++) {
+					System.out.print(" ");
+				}
+				for(int j=1; j<=4; j++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("--------------------------¡â13STAR");
+		
+
+		System.out.println("--------------------------¡â14STAR");
+		
+		
 	}
 }
---
+++ oct/src/oct27/test002.java
@@ -0,0 +1,14 @@
+package oct27;
+
+import java.util.Scanner;
+
+public class test002 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+
+
+	}
+
+}

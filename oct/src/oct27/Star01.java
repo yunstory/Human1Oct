89abfa5ed7d0924e8861acb671a5bc368d85dc10
USER <USER@USER-PC.local>
2020-10-27 17:37:28 +0900

    add

--- oct/src/oct27/Star01.java
+++ oct/src/oct27/Star01.java
@@ -247,8 +247,54 @@ public class Star01 {
 		}
 		System.out.println("--------------------------¡â13STAR");
 		
+		for(int i=1; i<=9; i++) {
+			for(int k=1; k<=9-i; k++) {
+				System.out.print(" ");
+			}
+			System.out.print(" ");
+			System.out.print("*");
+			if(i>1&&i<=8) {
+				for(int k=1; k<(i-1)*2; k++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+			}
+			if(i==9) {
+				for(int j=1; j<=9*2-2; j++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		
 
 		System.out.println("--------------------------¡â14STAR");
+		
+		for(int i=1; i<=9; i++) {
+			if(i<=5) {
+				for(int j=1; j<=i-1; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+				for(int j=1; j<=(5-i)*2; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+			}else {
+				for(int j=1; j<=9-i; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+				for(int j=1; j<=(i-5)*2; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("--------------------------¡â15STAR");
+		
 		
 		
 	}
--- oct/src/oct27/test002.java
+++ oct/src/oct27/test002.java
@@ -7,8 +7,50 @@ public class test002 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		
+		
+		for(int i=1; i<=100; i++) {
+			if(i/10==3||i/10==6||i/10==9) {
+				System.out.println("짝짝");
+			}else if(i%10==3||i%10==6||i%10==9) {
+				System.out.println("짝s");
+			}else {
+			System.out.println(i);
+			}
+		}
+		
+		
+		
+		
+		
+//		int num = (int)(Math.random()*9+1);
+//		int o = 0; 
+//		int x = 0;
+//		
+//
+//		while(true) {
+//			System.out.print("숫자입력 : ");
+//			int i = Integer.parseInt(sc.nextLine());
+//			
+//			if(i==num) {
+//				o++;
+//				System.out.println("정답입니다  \n");
+//				break;
+//			}else {
+//				System.out.print("오답입니다 ::");
+//				x++;
+//				if(i>num) {
+//					System.out.println("보다 작은 수를 입력해주세요\n");
+//				}else {
+//					System.out.println("보다 큰 수를 입력해주세요\n");
+//				}
+//			}
+//		}
+//		System.out.println("게임 총" + (o+x) + "회 번째에 맞춤");
+		
+		
 
-
+		
 	}
 
 }
